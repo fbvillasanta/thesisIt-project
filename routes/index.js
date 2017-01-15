b1edc199d6cf58dd717e0e22bffec5317910a02b
fbvillasanta <fbvillasanta@yahoo.com>
2017-01-16 03:29:18 +0800

    style

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var ObjectId = require('mongodb').ObjectId;
+var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+	var collection = db.get().collection('collection');
+	collection.find().sort({_id: -1}).limit(3).toArray(function(e, entry){
+	  console.log(entry);
+	  res.render('index', { title: 'Home', entries: entry});   
+	});
 });
 
 module.exports = router;
