3ebfccb78cf58e32ec59d5b7ed9d0e7fc253a4ce
fbvillasanta <fbvillasanta@yahoo.com>
2017-01-28 13:42:10 +0800

    register validation

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
 
 var datetime = require('node-datetime');
 var dt;
@@ -46,6 +52,47 @@ db.connect(mdbUrl, function(err) {
 		app.use(bodyParser.urlencoded({extended: false}));
 		app.use(cookieParser());
 		app.use(express.static(path.join(__dirname, 'public')));
+
+		// Express Session
+		app.use(session({
+		    secret: 'secret',
+		    saveUninitialized: true,
+		    resave: true
+		}));
+
+		// Passport init
+		app.use(passport.initialize());
+		app.use(passport.session());
+
+		// Express Validator
+		app.use(expressValidator({
+		  errorFormatter: function(param, msg, value) {
+		      var namespace = param.split('.')
+		      , root    = namespace.shift()
+		      , formParam = root;
+
+		    while(namespace.length) {
+		      formParam += '[' + namespace.shift() + ']';
+		    }
+		    return {
+		      param : formParam,
+		      msg   : msg,
+		      value : value
+		    };
+		  }
+		}));
+
+		// Connect Flash
+		app.use(flash());
+
+		// Global Vars
+		app.use(function (req, res, next) {
+		  res.locals.success_msg = req.flash('success_msg');
+		  res.locals.error_msg = req.flash('error_msg');
+		  res.locals.error = req.flash('error');
+		  res.locals.user = req.user || null;
+		  next();
+		});
 
 
 		app.use('/', index);
