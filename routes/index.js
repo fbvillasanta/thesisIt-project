583c10701a8747feee155b9bebea6361278af5ed
fbvillasanta <fbvillasanta@yahoo.com>
2017-03-25 00:20:05 +0800

    modify index GET, show by Department

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.use(methodOverride(function(req, res){
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	//var collection = db.get().collection('theses');
-	Thesis.find()
+	Thesis.find({department: req.user.department})
 		.sort({added: -1})
 		.limit(3).exec(function(e, entry){
 		  console.log(entry);
